eb7ff12e6fbd9a26a78075b90c2a76a5f7b7999d
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-11-25 15:25:10 +0100

    chmgj

--- chippo/src/components/footer/footer.tsx
+++ chippo/src/components/footer/footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
 
   return (
     <div className='footer-section' id='contact'>
-      <header className='footer-header'>Get some $cCHIPPO here</header>
+      <header className='footer-header'>Get some $CHIPPO here</header>
       <span>Ready to do this? Let&apos;s get rich</span>
       <button className='copy-button' onClick={handleCopy}>CONTRACT ADDRESS</button>
       <div className="footer-icons">
