c587702e39a7a130df91b69cedb2dcf379affec9
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-11-25 21:35:55 +0100

    commit

---
+++ gugo/src/app/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import styles from "./page.module.scss";
+import './globals.css'
+import Footer from "@/components/footer/footer";
+
+export default function Home() {
+
+  return (
+    <>
+    
+
+        <div className={styles.container}> 
+          <div className={styles.banner__section} id="home">
+              <div className="banner-bacground-image">
+                  <img
+                      className="rottate-image videobanner desktop"
+                      src="/IMG_3371.JPG" // Replace with the path to your video file
+
+                  />
+                  <img
+                      className="rottate-image videobanner mobile"
+                      src="/IMG_3369.JPG" // Replace with the path to your video file
+
+                  />
+              </div>
+            <div className={styles.banner__section__overlay}>
+                    <div className="header-divo">
+                        <h1 className="header-landing">
+                            $GUGO CTO
+                        </h1>
+                    </div>
+              <div className={styles.cta__buttons}>
+                <button className={styles.buy__button}><a style={{textDecoration:"none", color:"inherit"}} href="https://jup.ag/swap/SOL-jCHd8bHsn9XLkLpP6tS63infRFHhorzXpt7pjU5pump">Buy Now</a> </button>
+                <button className={styles.get__button}> <a style={{textDecoration:"none", color:"inherit"}} href="https://dexscreener.com/solana/a8yhjw5rxupz6nnrivtnh4ajnsfotv2aq64ubyqwzmxe">Get on Dex</a></button>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+          <Footer/>
+        </div>
+
+
+    </>
+  );
+}
---
+++ gugo/src/components/footer/footer.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React, { useState } from 'react';
+import './footer.scss';
+// import { FaTwitter } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaTelegramPlane } from 'react-icons/fa';
+import Image from 'next/image';
+import Dex from '../../../public/dex-screener-seeklogo.png'
+
+const Footer = () => {
+  const [copied, setCopied] = useState(false);
+  const code = "jCHd8bHsn9XLkLpP6tS63infRFHhorzXpt7pjU5pump";
+
+  const handleCopy = () => {
+    console.log('clicked');
+    navigator.clipboard.writeText(code).then(() => {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); 
+    });
+  };
+
+  return (
+    <div className='footer-section' id='contact'>
+      <header className='footer-header'>Get some $CHIPPO here</header>
+      <span>Ready to do this? Let&apos;s get rich</span>
+      <button className='copy-button' onClick={handleCopy}>CONTRACT ADDRESS</button>
+      <div className="footer-icons">
+          <div className="links">
+              <a href="https://x.com/ChippoSolana" className="">
+                  <FaXTwitter/>
+              </a>
+          </div>
+          <div className="links">
+              <a href="https://t.me/chipposolana" className="">
+                  <FaTelegramPlane />
+              </a>
+          </div>
+          <div className="links">
+              <a href="https://dexscreener.com/solana/8wgy9ke3yatcvoiw4engaxvukpqr2pmvhzcmyzavlbke" className="">
+                  <Image src={Dex} alt=''/>
+              </a>
+          </div>
+      </div>
+
+      <span className={`tooltip ${copied ? 'show' : ''}`}>Copied to clipboard</span>
+    </div>
+  );
+};
+
+export default Footer;
