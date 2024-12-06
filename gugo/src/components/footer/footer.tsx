ae0503c39b55a23c3fd4e46220d2c1f85db6dda8
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-12-06 11:56:43 +0100

    commit

--- gugo/src/components/footer/footer.tsx
+++ gugo/src/components/footer/footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
   return (
     <div className='footer-section' id='contact'>
       <header className='footer-header'>Get some $DOGE here</header>
-      <span>Ready to do this? Let&apos;s get rich</span>
+      <span className="white">Ready to do this? Let&apos;s get rich</span>
       <button className='copy-button' onClick={handleCopy}>CONTRACT ADDRESS</button>
       <div className="footer-icons">
           <div className="links">
