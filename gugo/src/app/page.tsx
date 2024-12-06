c3196a29bd18a8900039a42b0de342a48ca168dc
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-12-06 22:35:16 +0100

    commit

--- gugo/src/app/page.tsx
+++ gugo/src/app/page.tsx
@@ -15,38 +15,38 @@ export default function Home() {
               <div className="banner-bacground-image">
                   <img
                       className="rottate-image videobanner desktop"
-                      src="/IMG_3571.JPG" // Replace with the path to your video file
+                      src="photo_2024-12-06 21.41.35.jpeg" // Replace with the path to your video file
 
                   />
                   <img
                       className="rottate-image videobanner mobile"
-                      src="/IMG_3571.JPG" // Replace with the path to your video file
+                      src="/photo_2024-12-06 21.41.35.jpeg" // Replace with the path to your video file
 
                   />
               </div>
             <div className={styles.banner__section__overlay}>
                     <div className="header-divo">
                         <h1 className="header-landing">
-                            $DOGE OF COMFORT
+                            $GOATMAS CTO
                         </h1>
                     </div>
               <div className={styles.cta__buttons}>
-                <button className={styles.buy__button}><a style={{textDecoration:"none", color:"inherit"}} href="https://jup.ag/swap/SOL-Ea1tU16MedYE6McXLxVVv9iKYSkoPNGzJjef15UZpump">Buy Now</a> </button>
-                <button className={styles.get__button}> <a style={{textDecoration:"none", color:"inherit"}} href="https://dexscreener.com/solana/b4mbqj8ay6aqrbdypu3esgd9e2lhmzazkbrwg3vtc6x9">Get on Dex</a></button>
+                <button className={styles.buy__button}><a style={{textDecoration:"none", color:"inherit"}} href="https://jup.ag/swap/SOL-Ekir7xTBxsQLzdAy5SGJ6kfWqcRNMD39ERaojoodpump">Buy Now</a> </button>
+                <button className={styles.get__button}> <a style={{textDecoration:"none", color:"inherit"}} href="https://dexscreener.com/solana/cijxn2fkxqghlwczucdhfwssnhl8npqye4y8ta6rvxc3">Get on Dex</a></button>
               </div>
             </div>
             <div>
                 <div className={styles.meme__gallery} id="gallery">
                     <div className={styles.meme}>
-                        <img src="/IMG_3567.JPG" alt="#"/>
+                        <img src="/photo_2024-12-06 21.41.50.jpeg" alt="#"/>
                     </div>
 
                     <div className={styles.meme}>
-                        <img src="/IMG_3568.JPG" alt="#"/>
+                        <img src="/photo_2024-12-06 21.41.56.jpeg" alt="#"/>
                     </div>
 
                     <div className={styles.meme}>
-                        <img src="/IMG_3569.JPG" alt="#"/>
+                        <img src="/photo_2024-12-06 21.42.00.jpeg" alt="#"/>
                     </div>
 
                 </div>
--- gugo/src/components/footer/footer.tsx
+++ gugo/src/components/footer/footer.tsx
@@ -9,7 +9,7 @@ import Dex from '../../../public/dex-screener-seeklogo.png'
 
 const Footer = () => {
   const [copied, setCopied] = useState(false);
-  const code = "Ea1tU16MedYE6McXLxVVv9iKYSkoPNGzJjef15UZpump";
+  const code = "Ekir7xTBxsQLzdAy5SGJ6kfWqcRNMD39ERaojoodpump";
 
   const handleCopy = () => {
     console.log('clicked');
@@ -21,22 +21,22 @@ const Footer = () => {
 
   return (
     <div className='footer-section' id='contact'>
-      <header className='footer-header'>Get some $DOC here</header>
+      <header className='footer-header'>Get some $GOAT here</header>
       <span className="white">Ready to do this? Let&apos;s get rich</span>
       <button className='copy-button' onClick={handleCopy}>CONTRACT ADDRESS</button>
       <div className="footer-icons">
           <div className="links">
-              <a href="https://x.com/DOC_CTO" className="">
+              <a href="https://x.com/GOATMASCTO" className="">
                   <FaXTwitter/>
               </a>
           </div>
           <div className="links">
-              <a href="https://t.me/DogeOfComfortCTO" className="">
+              <a href="https://t.me/GoatmasCTO" className="">
                   <FaTelegramPlane />
               </a>
           </div>
           <div className="links">
-              <a href="https://dexscreener.com/solana/b4mbqj8ay6aqrbdypu3esgd9e2lhmzazkbrwg3vtc6x9" className="">
+              <a href="https://dexscreener.com/solana/cijxn2fkxqghlwczucdhfwssnhl8npqye4y8ta6rvxc3" className="">
                   <Image src={Dex} alt=''/>
               </a>
           </div>
